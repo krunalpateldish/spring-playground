b56cd0a9a3eea3082fdf18556864251513c020ec
trainer #2 <trainer2@trainers-MacBook-Pro.local>
2017-06-08 11:41:15 -0600

    final commit

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -6,9 +6,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
-    public String helloWorld() {
-        return "Hello from Spring!";
+//    @GetMapping("/")
+//    public String helloWorld() {
+//        return "Hello from Spring!";
+//    }
+
+
+    @GetMapping("/math/pi")
+    public String mathPi(){
+        return "3.141592653589793";
     }
+
 
 }
